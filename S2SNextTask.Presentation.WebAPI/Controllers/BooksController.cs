4c60098aab907a02b4fe78dc79c0c78af0df3b20
WOTTAKWOT <aywee26@gmail.com>
2022-08-11 18:44:34 +0300

    Basic implementation of WebAPI.

---
+++ S2SNextTask.Presentation.WebAPI/Controllers/BooksController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using S2SNextTask.Application.BooksService.Queries;
+using S2SNextTask.Domain.Entities;
+
+namespace S2SNextTask.Presentation.WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BooksController : Controller
+{
+    private readonly ISender _mediator;
+
+    public BooksController(ISender mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<Book>> GetAllBooks()
+    {
+        var result = await _mediator.Send(new GetAllBooksQuery());
+        return result;
+    }
+}
