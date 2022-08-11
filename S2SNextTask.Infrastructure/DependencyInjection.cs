54da595e43f1c13516c55a4dc9cc1cffc2140d25
WOTTAKWOT <aywee26@gmail.com>
2022-08-11 18:08:03 +0300

    Basic Infrastructure implementation

---
+++ S2SNextTask.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Ardalis.GuardClauses;
+using Microsoft.Extensions.DependencyInjection;
+using S2SNextTask.Application.Common.Interfaces.Persistence;
+using S2SNextTask.Infrastructure.Persistence.Repositories;
+
+namespace S2SNextTask.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastucture(this IServiceCollection services)
+    {
+        Guard.Against.Null(services, nameof(services));
+
+        services.AddScoped<IBooksRepository, BooksRepository>();
+
+        return services;
+    }
+}
---
+++ S2SNextTask.Infrastructure/Persistence/Repositories/BooksRepository.cs
@@ -0,0 +1,25 @@
+﻿using S2SNextTask.Application.Common.Interfaces.Persistence;
+using S2SNextTask.Domain.Entities;
+
+namespace S2SNextTask.Infrastructure.Persistence.Repositories
+{
+    public class BooksRepository : IBooksRepository
+    {
+        public Task<IEnumerable<Book>> GetAllBooksAsync(CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Book?> GetBookByIdAsync(Guid id, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Book?> DeleteBookAsync(Book entity, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
